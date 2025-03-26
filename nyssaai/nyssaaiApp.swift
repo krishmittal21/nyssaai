c331a5647efefb18cf9386976dce456f34c3fdc1
Krish Mittal <114004042+krishmittal21@users.noreply.github.com>
2025-03-26 13:04:03 +0530

    Initial Commit

---
+++ nyssaai/nyssaaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  nyssaaiApp.swift
+//  nyssaai
+//
+//  Created by Krish Mittal on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct nyssaaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
